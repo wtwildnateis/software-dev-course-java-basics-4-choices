79655698f54642c4745b3ffd3e8e6d02ee76121d
wtwildnateis <wtwildnateis@gmail.com>
2025-08-14 19:40:22 -0500

    Updated assignment for LaunchCode

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 // For these exercises, you need to declare a single variable
 // in each function that uses the appropriate collection type
 // (Array, ArrayList, HashSet, HashMap)
@@ -19,6 +23,7 @@ public class CollectionChoiceExercises {
     public void MakeStudentCollection() {
         // Create an appropriate collection variable for holding a list of student names
         // that will be added to and removed from during program execution.
+        ArrayList<String> students;
     }
 
     public void MakeLoggedInUsersCollection() {
@@ -26,11 +31,13 @@ public class CollectionChoiceExercises {
         // unique logged in usernames where adding a username that's already
         // present is ignored and checking if a user is in the collection can
         // be done very quickly.
+        HashSet<String> loggedIn;
     }
 
     public void MakeEmployeeAccessCodesCollection() {
         // Create an appropriate collection variable for holding a set of
         // key-value pairs where they key is an employee name (String) and the
         // value is an employee access code (String.)
+        HashMap<String, String> employeeAccessCodes;
     }
 }
